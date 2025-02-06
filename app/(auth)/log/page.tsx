9745d233bb16b683cffae204f7847de973129761
Callmesammy <callmesammy@yahoo.com>
2025-02-06 11:41:00 -0800

    update

--- app/(auth)/log/page.tsx
+++ app/(auth)/log/page.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
-import Auth from "./page";
 
 export default function Login() {
   return (
     <div className=" w-full h-full flex items-center justify-center px- ">
       <Image src="/ai.avif" alt="ai image " fill className=" object-fill opacity-25"/>
-      <Auth/>
     </div>
   );
 }
